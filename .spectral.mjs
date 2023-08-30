fbaab74e398611c1ef8e1f4bd9f99c3932c2e246
qa+github@stoplight.io <qa+github@stoplight.io>
2023-08-30 18:39:40 -0400

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://integration.stoplight-dev.com/api/v1/projects/cHJqOjMzNzA0MQ/spectral.js?branch_slug=main&token=001aab68-38f9-47d7-a5f2-7dbd9721d2d6";
+export default { extends: ruleset };
